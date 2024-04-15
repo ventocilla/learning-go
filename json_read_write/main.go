fe152e2272bfeff32a559215c6f1809ad1178a09
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-04-15 20:59:35 -0300

    final code reading and writing to json

--- json_read_write/main.go
+++ json_read_write/main.go
@@ -30,10 +30,34 @@ func main() {
 	]
 	`
 
+	// unMarshal => Json to Struct
 	var unmarshalled []Person
 	err := json.Unmarshal([]byte(myJson), &unmarshalled)
 	if err != nil {
 		fmt.Println("Error unmarshalling json", err)
 	}
 	fmt.Printf("unMarsheled: %v", unmarshalled)
+
+	// Write json from a struct, Marshal => struct to Json
+	var mySlice []Person
+	var m1 Person
+	var m2 Person
+
+	m1.Firstname = "Wally"
+	m1.Lastname = "West"
+	m1.HairColor = "red"
+	m1.HasDog = false
+
+	m2.Firstname = "Diana"
+	m2.Lastname = "Prince"
+	m2.HairColor = "black"
+	m2.HasDog = false
+
+	mySlice = append(mySlice, m1)
+	mySlice = append(mySlice, m2)
+	newJson, err := json.MarshalIndent(mySlice, "", "     ")
+	if err != nil {
+		fmt.Println("error marshalling", err)
+	}
+	fmt.Println(string(newJson))
 }
